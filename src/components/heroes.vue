6e477ed7f675f941416cdc53368e72ab8ced43a1
Heather Cunningham <hcunningham@patientkeeper.com>
2022-07-10 17:36:12 -0400

    chap3: two-way binding data with v-model and bind inline style

--- src/components/heroes.vue
+++ src/components/heroes.vue
@@ -17,31 +17,31 @@
               </div>
               <div class="field">
                 <label class="label" for="firstName">first name</label>
-                <input class="input" id="firstName"/>
+                <input class="input" id="firstName" v-model="hero.firstName"/>
               </div>
               <div class="field">
                 <label class="label" for="lastName">last name</label>
-                <input class="input" id="lastName"/>
+                <input class="input" id="lastName" v-model="hero.lastName"/>
               </div>
               <div class="field">
                 <label class="label" for="description">description</label>
-                <textarea class="input" id="description" type="text"/>
+                <textarea class="input" id="description" type="text" v-model="hero.description"/>
               </div>
               <div class="field">
                 <label class="label">cape color</label>
                 <label class="radio" for="color-red">
-                  <input type="radio" id="color-red" value="red"/>
+                  <input type="radio" id="color-red" value="red" v-model="hero.capeColor"/>
                   red
                 </label>
                 <label class="radio" for="color-blue">
-                  <input type="radio" id="color-blue" value="blue"/>
+                  <input type="radio" id="color-blue" value="blue" v-model="hero.capeColor"/>
                   blue
                 </label>
                 <label class="radio" for="color-green">
-                  <input type="radio" id="color-green" value="green"/>
+                  <input type="radio" id="color-green" value="green" v-model="hero.capeColor"/>
                   green
                 </label>
-                <div class="color-line"></div>
+                <div class="color-line" :style="{'background-color': hero.capeColor}"></div>
               </div>
               <div class="field">
                 <label for="power">
@@ -60,7 +60,7 @@
               <div class="field">
                 <label class="checkbox" for="active">
                   active
-                  <input type="checkbox" class="is-primary" id="active"/>
+                  <input type="checkbox" class="is-primary" id="active" v-model="hero.active"/>
                 </label>
               </div>
             </div>
@@ -95,7 +95,7 @@ export default {
         lastName: 'Papa',
         description: 'cat whisperer',
         capeColor: '',
-        power: 'cat whisperer',
+        power: '',
         active: 'true'
       },
       message: "",
